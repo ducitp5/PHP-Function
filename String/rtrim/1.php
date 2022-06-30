b8872e579e8d193b2a32f6da7eae8172edc5050c
ductt <ducitp5@gmail.com>
2022-06-30 15:17:30 +0700

    $_SERVER

---
+++ String/rtrim/1.php
@@ -0,0 +1,22 @@
+<?php
+
+$text = "\t\tThese are a few words :) ...  ";
+$binary = "\x09Example string\x0A";
+$hello  = "abcdefgh";
+var_dump($text, $binary, $hello);
+
+print "\n";
+
+$trimmed = rtrim($text);
+var_dump($trimmed);
+
+$trimmed = rtrim($text, " \t.");
+var_dump($trimmed);
+
+$trimmed = rtrim($hello, "egathf");
+var_dump($trimmed);
+
+// trim the ASCII control characters at the end of $binary
+// (from 0 to 31 inclusive)
+$clean = rtrim($binary, "\x00..\x1F");
+var_dump($clean);
