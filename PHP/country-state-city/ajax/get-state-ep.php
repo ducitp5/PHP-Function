f93abc2d932388ca433796aa6bc5845b4b3c3cc3
duc tran <duceduca2022@gmail.com>
2022-07-14 18:42:28 +0700

    Directory PHP && javascript

---
+++ PHP/country-state-city/ajax/get-state-ep.php
@@ -0,0 +1,27 @@
+<?php
+    
+    namespace Phppot;
+    
+    require_once __DIR__ . '/../Model/CountryStateCity.php';
+    
+    if (! empty($_POST["country_id"])) {
+        
+        $countryId              =    $_POST["country_id"];
+        
+        
+        
+        $countryStateCity       =    new CountryStateCity();
+        
+        $stateResult            =    $countryStateCity      ->getStateByCountrId( $countryId );
+?>
+    <option value="">Select State</option>
+
+<?php
+
+        foreach ($stateResult as $state) {
+?>
+    <option value="<?php echo $state["id"]; ?>"><?php echo $state["name"]; ?></option>
+<?php
+        }
+    }
+?>
