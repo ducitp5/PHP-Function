5aca4dc14017091077d53a8f700c4d0b988b18be
Ductt <duceduca2022@gmail.com>
2022-11-29 10:00:20 +0700

    <todo-item>

--- vi-vuejs-org/4_computed-watcher.php
+++ vi-vuejs-org/4_computed-watcher.php
@@ -51,55 +51,55 @@
 
 
 
-<hr>
-<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
-
-<div id="watch-example">
-    <p>
-        Hãy hỏi một câu hỏi yes/no: <input v-model="question">
-    </p>
-    <p>{{ answer }}</p>
-</div>
-
-<script>
-    var watchExampleVM = new Vue({
-        el: '#watch-example',
-        data: {
-            question: '',
-            answer: '11111'
-        },
-        watch: {
-            // bất cứ lúc nào câu hỏi thay đổi, hàm bên dưới sẽ chạy
-            question: function (newQuestion, oldQuestion) {
-                this.answer = '22222222...'
-                this.getAnswer()
-            }
-        },
-        methods: {
-            // _.debounce là một hàm do Lodash cung cấp
-            // Để tìm hiểu rõ hơn cách hoạt động của hàm này,
-            // bạn có thể truy cập: https://lodash.com/docs#debounce
-            getAnswer: _.debounce(
-                function () {
-                    if (this.question.indexOf('?') === -1) {
-                        this.answer = 'Câu hỏi thì thường chứa một dấu "?" '
-                        return
-                    }
-                    this.answer = 'Đang suy nghĩ...'
-                    var vm = this
-                    axios.get('https://yesno.wtf/api')
-                        .then(function (response) {
-                            vm.answer = _.capitalize(response.data.answer)
-                        })
-                        .catch(function (error) {
-                            vm.answer = 'Lỗi! Không thể truy cập API. ' + error
-                        })
-                },
-                // Đây là thời gian (đơn vị mili giây) chúng ta đợi người dùng dừng gõ.
-                500
-            )
-        }
-    })
-</script>
+<!--<hr>-->
+<!--<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>-->
+<!--<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>-->
+<!---->
+<!--<div id="watch-example">-->
+<!--    <p>-->
+<!--        Hãy hỏi một câu hỏi yes/no: <input v-model="question">-->
+<!--    </p>-->
+<!--    <p>{{ answer }}</p>-->
+<!--</div>-->
+<!---->
+<!--<script>-->
+<!--    var watchExampleVM = new Vue({-->
+<!--        el: '#watch-example',-->
+<!--        data: {-->
+<!--            question: '',-->
+<!--            answer: '11111'-->
+<!--        },-->
+<!--        watch: {-->
+<!--            // bất cứ lúc nào câu hỏi thay đổi, hàm bên dưới sẽ chạy-->
+<!--            question: function (newQuestion, oldQuestion) {-->
+<!--                this.answer = '22222222...'-->
+<!--                this.getAnswer()-->
+<!--            }-->
+<!--        },-->
+<!--        methods: {-->
+<!--            // _.debounce là một hàm do Lodash cung cấp-->
+<!--            // Để tìm hiểu rõ hơn cách hoạt động của hàm này,-->
+<!--            // bạn có thể truy cập: https://lodash.com/docs#debounce-->
+<!--            getAnswer: _.debounce(-->
+<!--                function () {-->
+<!--                    if (this.question.indexOf('?') === -1) {-->
+<!--                        this.answer = 'Câu hỏi thì thường chứa một dấu "?" '-->
+<!--                        return-->
+<!--                    }-->
+<!--                    this.answer = 'Đang suy nghĩ...'-->
+<!--                    var vm = this-->
+<!--                    axios.get('https://yesno.wtf/api')-->
+<!--                        .then(function (response) {-->
+<!--                            vm.answer = _.capitalize(response.data.answer)-->
+<!--                        })-->
+<!--                        .catch(function (error) {-->
+<!--                            vm.answer = 'Lỗi! Không thể truy cập API. ' + error-->
+<!--                        })-->
+<!--                },-->
+<!--                // Đây là thời gian (đơn vị mili giây) chúng ta đợi người dùng dừng gõ.-->
+<!--                500-->
+<!--            )-->
+<!--        }-->
+<!--    })-->
+<!--</script>-->
 
