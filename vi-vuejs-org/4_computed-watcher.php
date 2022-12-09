10399e40a1c332d6af6218fa3b6fe085b5cee682
Ductt <duceduca2022@gmail.com>
2022-12-09 20:37:04 +0700

    use component

--- vi-vuejs-org/4_computed-watcher.php
+++ vi-vuejs-org/4_computed-watcher.php
@@ -3,6 +3,8 @@
 <!--<div id="example">-->
 <!--    <p>Thông điệp ban đầu: "{{ message }}"</p>-->
 <!--    <p>(computed): "{{ reversedMessage }}"</p>-->
+<!---->
+<!--    <p>(computed): "{{ message }}"</p>-->
 <!--</div>-->
 <!---->
 <!--<script>-->
@@ -39,67 +41,106 @@
 <!--        },-->
 <!--        watch: {-->
 <!--            firstName: function (val) {-->
-<!--                this.fullName = val + ' ' + this.lastName-->
+<!--                this.fullName = val + ' - ' + this.lastName-->
 <!--            },-->
 <!--            lastName: function (val) {-->
-<!--                this.fullName = this.firstName + ' ' + val-->
+<!--                this.fullName = this.firstName + ' - ' + val-->
 <!--            }-->
 <!--        }-->
 <!--    })-->
 <!--</script>-->
-
-
 
 
 <!--<hr>-->
-<!--<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>-->
-<!--<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>-->
-<!---->
-<!--<div id="watch-example">-->
-<!--    <p>-->
-<!--        Hãy hỏi một câu hỏi yes/no: <input v-model="question">-->
-<!--    </p>-->
-<!--    <p>{{ answer }}</p>-->
+<!--<div id="demo">-->
+<!--    <p>{{ firstName }}</p>-->
+<!--    <p>{{ lastName }}</p>-->
+<!--    <p>{{ fullName }}</p>-->
 <!--</div>-->
 <!---->
 <!--<script>-->
-<!--    var watchExampleVM = new Vue({-->
-<!--        el: '#watch-example',-->
+<!--    var vm = new Vue({-->
+<!--        el: '#demo',-->
 <!--        data: {-->
-<!--            question: '',-->
-<!--            answer: '11111'-->
+<!--            firstName: 'Evan as',-->
+<!--            lastName: 'You',-->
+<!--            fullName: 'duc tran'-->
 <!--        },-->
-<!--        watch: {-->
-<!--            // bất cứ lúc nào câu hỏi thay đổi, hàm bên dưới sẽ chạy-->
-<!--            question: function (newQuestion, oldQuestion) {-->
-<!--                this.answer = '22222222...'-->
-<!--                this.getAnswer()-->
-<!--            }-->
-<!--        },-->
-<!--        methods: {-->
-<!--            // _.debounce là một hàm do Lodash cung cấp-->
-<!--            // Để tìm hiểu rõ hơn cách hoạt động của hàm này,-->
-<!--            // bạn có thể truy cập: https://lodash.com/docs#debounce-->
-<!--            getAnswer: _.debounce(-->
-<!--                function () {-->
-<!--                    if (this.question.indexOf('?') === -1) {-->
-<!--                        this.answer = 'Câu hỏi thì thường chứa một dấu "?" '-->
-<!--                        return-->
-<!--                    }-->
-<!--                    this.answer = 'Đang suy nghĩ...'-->
-<!--                    var vm = this-->
-<!--                    axios.get('https://yesno.wtf/api')-->
-<!--                        .then(function (response) {-->
-<!--                            vm.answer = _.capitalize(response.data.answer)-->
-<!--                        })-->
-<!--                        .catch(function (error) {-->
-<!--                            vm.answer = 'Lỗi! Không thể truy cập API. ' + error-->
-<!--                        })-->
+<!--        computed: {-->
+<!--            // fullName: function () {-->
+<!--            //     return this.firstName + ' ' + this.lastName-->
+<!--            // }-->
+<!---->
+<!--            fullName2: {-->
+<!--                // getter-->
+<!--                get: function () {-->
+<!--                    return this.firstName + ' --- ' + this.lastName-->
 <!--                },-->
-<!--                // Đây là thời gian (đơn vị mili giây) chúng ta đợi người dùng dừng gõ.-->
-<!--                500-->
-<!--            )-->
+<!--                // setter-->
+<!--                set: function (newValue) {-->
+<!--                    var names = newValue.split(' ')-->
+<!--                    this.firstName = names[0]-->
+<!--                    this.lastName = names[names.length - 1];-->
+<!--                    console.log(newValue);-->
+<!--                }-->
+<!--            }-->
 <!--        }-->
 <!--    })-->
+<!---->
+<!--    vm.fullName = 'John ala Doe'-->
 <!--</script>-->
+
+
+
+<div id="watch-example">
+    <p>
+        Hãy hỏi một câu hỏi yes/no:
+        <input v-model="question">
+    </p>
+    <p>{{ answer }}</p>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
+<script>
+    var watchExampleVM = new Vue({
+        el: '#watch-example',
+        data: {
+            question: '',
+            answer: 'Không thể trả lời nếu bạn chưa đặt câu hỏi!'
+        },
+        watch: {
+            // bất cứ lúc nào câu hỏi thay đổi, hàm bên dưới sẽ chạy
+            question: function (newQuestion, oldQuestion) {
+                this.answer = 'Đang chờ bạn đặt xong câu hỏi...'
+                this.getAnswer()
+            }
+        },
+        methods: {
+            // _.debounce là một hàm do Lodash cung cấp
+            // Để tìm hiểu rõ hơn cách hoạt động của hàm này,
+            // bạn có thể truy cập: https://lodash.com/docs#debounce
+            getAnswer: _.debounce(
+                function () {
+                    if (this.question.indexOf('?') === -1) {
+                        this.answer = 'Câu hỏi thì thường chứa một dấu "?" ;-)'
+                        return
+                    }
+                    console.log(123);
+                    // this.answer = 'Đang suy nghĩ...'
+                    var vm = this
+                    axios.get('https://yesno.wtf/api')
+                        .then(function (response) {
+                            vm.answer = _.capitalize(response.data.answer)
+                            console.log(response)
+                        })
+                        .catch(function (error) {
+                            vm.answer = 'Lỗi! Không thể truy cập API. ' + error
+                        })
+                },
+                2000
+            )
+        }
+    })
+</script>
 
