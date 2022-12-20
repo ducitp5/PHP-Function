91ec08ab2fbc368ac5c362acc4c55e50ee045386
Ductt <duceduca2022@gmail.com>
2022-12-20 15:29:43 +0700

    format code

--- codepen/Todo List UI with VueJs/myScript.js
+++ codepen/Todo List UI with VueJs/myScript.js
@@ -1,21 +1,29 @@
 Vue.component('togglebutton', {
+
     props: ['label', 'name'],
-    template: `<div class="togglebutton-wrapper" v-bind:class="isactive ? 'togglebutton-checked' : ''">
-      <label v-bind:for="name">
-        <span class="togglebutton-label">{{ label }}</span>
-        <span class="tooglebutton-box"></span>
-      </label>
-      <input v-bind:id="name" type="checkbox" v-bind:name="name" v-model="isactive" v-on:change="onToogle">
-  </div>`,
+
+    template:
+        `<div class="togglebutton-wrapper" v-bind:class="isactive ? 'togglebutton-checked' : ''">
+        
+          <label v-bind:for="name">
+            <span class="togglebutton-label">{{ label }}</span>
+            <span class="tooglebutton-box"></span>
+          </label>
+        
+          <input v-bind:id="name" type="checkbox" v-bind:name="name" v-model="isactive" v-on:change="onToogle">
+        </div>`,
+
     model: {
         prop: 'checked',
         event: 'change'
     },
+
     data: function() {
         return {
-            isactive:false
+            isactive : false
         }
     },
+
     methods: {
         onToogle: function() {
             this.$emit('clicked', this.isactive)
@@ -24,32 +32,45 @@ Vue.component('togglebutton', {
 });
 
 var todolist = new Vue({
+
     el: '#todolist',
+
     data: {
         newitem:'',
         sortByStatus:false,
         todo: [
-            { id:1, label: "Learn VueJs", done: true },
-            { id:2, label: "Code a todo list", done: false },
-            { id:3, label: "Learn something else", done: false }
+            { id:1, label: "Learn VueJs",            done: true },
+            { id:2, label: "Code a todo list",       done: false },
+            { id:3, label: "Learn something else",   done: false }
         ]
     },
+
     methods: {
         addItem: function() {
-            this.todo.push({id: Math.floor(Math.random() * 9999) + 10, label: this.newitem, done: false});
+            this.todo.push(
+                {
+                    id      : Math.floor(Math.random() * 9999) + 10,
+                    label   : this.newitem,
+                    done    : false
+                }
+            );
             this.newitem = '';
         },
+
         markAsDoneOrUndone: function(item) {
             item.done = !item.done;
         },
+
         deleteItemFromList: function(item) {
             let index = this.todo.indexOf(item)
             this.todo.splice(index, 1);
         },
+
         clickontoogle: function(active) {
             this.sortByStatus = active;
         }
     },
+
     computed: {
         todoByStatus: function() {
 
@@ -57,12 +78,15 @@ var todolist = new Vue({
                 return this.todo;
             }
 
-            var sortedArray = []
-            var doneArray = this.todo.filter(function(item) { return item.done; });
-            var notDoneArray = this.todo.filter(function(item) { return !item.done; });
+            var sortedArray     = []
+            var doneArray       = this.todo.filter(function(item) { return item.done; });
+            var notDoneArray    = this.todo.filter(function(item) { return !item.done; });
 
-            sortedArray = [...notDoneArray, ...doneArray];
+            sortedArray         = [...notDoneArray, ...doneArray];
+
+            console.log(sortedArray)
             return sortedArray;
         }
     }
 });
+
