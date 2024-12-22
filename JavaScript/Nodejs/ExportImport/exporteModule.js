088e988eb212759898a3ebf4e9d24bcd08261e1f
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2024-12-23 01:08:06 +0700

    import - export in node.js

---
+++ JavaScript/Nodejs/ExportImport/exporteModule.js
@@ -0,0 +1,18 @@
+// module.exports = 'xx'
+//
+// module.exports = {
+//     a : "A",
+//
+//     sayHelloInEnglish: function() {
+//         return "HELLO";
+//     },
+// };
+//
+// module.exports.y = "yyy"
+//
+// let $t = "TT";
+// module.exports.t = $t;
+
+exports.x = "xxx"
+exports.y = "yyy"
+
---
+++ JavaScript/Nodejs/ExportImport/importModule.js
@@ -0,0 +1,9 @@
+console.log(222222);
+let greetings = require("./exporteModule.js");
+
+console.log(greetings);
+// console.log(greetings.sayHelloInEnglish());
+console.log(greetings.a);
+// //
+// console.log(greetings.x);
+
